dd5a7bd7eb98a6dd512a9c7f8719d71cbe863fbf
kavana <kavananavada03@gmail.com>
2025-07-31 23:48:23 +0530

    made responsive

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -83,11 +83,11 @@ function Main() {
             <div>
                 <img src={assets.gallery_icon} alt="" />
                 <img src={assets.mic_icon} alt="" />
-                <img  onClick={()=>onSent()} src={assets.send_icon} alt="" />
+                {input?<img  onClick={()=>onSent()} src={assets.send_icon} alt="" />:null}
             </div>
         </div>
         <p className="bottom-info">
-            Information goes here
+           
         </p>
       </div>
       </div>
